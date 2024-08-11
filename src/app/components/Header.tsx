15ea602cf0f65973a250eb4fa80b865386fc1536
nalbertysales <nalbertysales6@gmail.com>
2024-08-11 13:06:15 -0300

    feat: add Header component to app layout

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <>
+      <header className="h-28 w-full flex justify-between px-24 items-center">
+        <div>
+          <img src="/Clothes-Store-logo.svg" alt="Clothes Store" />
+        </div>
+
+        <ul className="flex gap-12 items-center">
+          <li>Shop</li>
+
+          <li>Who we are</li>
+
+          <li>My profile</li>
+
+          <li>
+            <button className="bg-emerald-300 px-8 py-3 rounded-lg">
+              <img src="/cart.svg" alt="Cart" />
+            </button>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default Header;
