3d6f7543a427387eb599e8ce80599c3fc4b2076d
nalbertysales <nalbertysales6@gmail.com>
2024-08-17 10:09:40 -0300

    feat: add Search component to layout

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,14 @@
+const Search = () => {
+  return (
+    <ul className="flex gap-48 justify-center pt-4">
+      <li>Woman</li>
+      <li>Man</li>
+      <li>Kids</li>
+      <li>Jeans</li>
+      <li>Shoes</li>
+      <li>Sports</li>
+    </ul>
+  );
+};
+
+export default Search;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <Search />
         {children}
       </body>
     </html>
