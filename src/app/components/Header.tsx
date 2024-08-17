ba7613ecdcc0a0a90020bd1dbaa762ae0f8c459a
nalbertysales <nalbertysales6@gmail.com>
2024-08-17 10:11:14 -0300

    feat: add shopping bag button to Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,40 +1,44 @@
-import { Search } from "lucide-react";
+import { Search, ShoppingBag } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const Header = () => {
   return (
     <>
-      <header className="h-28 w-full flex justify-between px-24 items-center gap-12">
-        <div>
+      <header className="h-28 w-full flex px-24 items-center gap-2 flex-col justify-center">
+        <div className="w-full flex justify-between">
           <img src="/Clothes-Store-logo.svg" alt="Clothes Store" />
+
+          <ul className="flex gap-10 items-center">
+            <li className="flex border rounded-lg">
+              <Input className="border-none shadow-none focus-visible:border-none focus-visible:ring-0" />
+
+              <Button
+                className="border-none shadow-none"
+                variant="outline"
+                size="icon"
+              >
+                <Search />
+              </Button>
+            </li>
+
+            <li>Shop</li>
+
+            <li>Who we are</li>
+
+            <li>My profile</li>
+
+            <li>
+              <Button
+                className="border-none shadow-none hover:bg-slate-300 w-16 h-12"
+                variant="outline"
+                size="icon"
+              >
+                <ShoppingBag />
+              </Button>
+            </li>
+          </ul>
         </div>
-
-        <ul className="flex gap-12 items-center">
-          <li className="flex border rounded-lg">
-            <Input className="border-none shadow-none focus-visible:border-none focus-visible:ring-0" />
-
-            <Button
-              className="border-none shadow-none"
-              variant="outline"
-              size="icon"
-            >
-              <Search />
-            </Button>
-          </li>
-
-          <li>Shop</li>
-
-          <li>Who we are</li>
-
-          <li>My profile</li>
-
-          <li>
-            <button className="bg-emerald-300 px-8 py-3 rounded-lg">
-              <img src="/cart.svg" alt="Cart" />
-            </button>
-          </li>
-        </ul>
       </header>
     </>
   );
